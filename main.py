325ca43a33703b784d46226568069184cf1ca565
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2023-03-16 10:48:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from cams import Attendance as atd
 
-embeddings = "\embeddings\faces_embeddings.npz"
-model = "\models\svm_model_v1.pkl"
-database = "\database\records.csv"
+embeddings = "embeddings/faces_embeddings.npz"
+model = "models/svm_model_v1.pkl"
+database = "database/records.csv"
 
 ashesi = atd(database=database,
              model=model,
